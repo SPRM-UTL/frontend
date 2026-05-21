3fe61d26af6dbaab03cf71507cdb470fbf3daf31
JosueArmandoRivera <125640131+JosueArmandoRivera@users.noreply.github.com>
2026-05-20 23:00:14 -0600

    Se concectó exitosamente el frontend del login con el backend de la bd, se creó la carpeta service para manejar los servicios de auth y que login.ts los pudiera consumir. Y en el backend fue necesario habilitar cors modificando el progrm.cs en el backend para poder comunicar el https del back, con el http del front, ya que tienen protocolos y puertos diferentes

--- src/app/login/login.ts
+++ src/app/login/login.ts
@@ -1,17 +1,43 @@
 import { Component } from '@angular/core';
 import { RouterLink } from '@angular/router';
 import { Router } from '@angular/router';
+import { FormsModule } from '@angular/forms';
+
+import { AuthService } from '../services/auth.service';
 
 @Component({
   selector: 'app-login',
-  imports: [RouterLink],
+  imports: [RouterLink, FormsModule],
   templateUrl: './login.html',
   styleUrl: './login.css'
 })
 export class Login {
-  constructor(private router: Router) { }
+
+  correo: string = '';
+  contrasenia: string = '';
+
+  constructor(
+    private router: Router,
+    private authService: AuthService
+  ) { }
 
   onLogin() {
-    this.router.navigate(['/dashboard']);
+
+    this.authService.login(this.correo, this.contrasenia)
+      .subscribe({
+        next: (response) => {
+
+          console.log(response);
+
+          localStorage.setItem('token', response.token);
+
+          this.router.navigate(['/dashboard']);
+        },
+
+        error: (error) => {
+          console.error(error);
+          alert('Credenciales inválidas');
+        }
+      });
   }
 }
---
+++ src/app/services/auth.service.ts
@@ -0,0 +1,24 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class AuthService {
+
+  //private apiUrl = 'https://localhost:7068/api/Auth';
+  private apiUrl = 'https://localhost:7299/api/Auth';
+
+  constructor(private http: HttpClient) { }
+
+  login(correo: string, contrasenia: string): Observable<any> {
+
+    const body = {
+      correo,
+      contrasenia
+    };
+
+    return this.http.post(`${this.apiUrl}/login`, body);
+  }
+}
