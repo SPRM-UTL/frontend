9e41038a7afbe6a8b90e92466e7117e9abd2c256
IDGS-803-23000643 <IDGS-803-23000643@outlook.com>
2026-06-12 12:43:53 -0600

    cambios de barra de navegación 12/06/2026

--- src/app/services/auth.service.ts
+++ src/app/services/auth.service.ts
@@ -9,14 +9,14 @@ import { isPlatformBrowser } from '@angular/common';
 })
 export class AuthService {
   private platformId = inject(PLATFORM_ID);
-  private apiUrl = 'http://localhost:5295/api/Auth';
-
+  //private apiUrl = 'http://localhost:5295/api/Auth';
+    private apiUrl = 'https://backend-neao.onrender.com/api/Auth';
   // En tu AuthService.ts
   constructor(private http: HttpClient, private router: Router) {
     // Ejecuta la verificación cada 60 segundos automáticamente
     setInterval(() => {
       this.checkTokenExpiration();
-    }, 60000); 
+    }, 60000);
   }
   // En tu AuthService.ts
   login(correo: string, contrasenia: string): Observable<any> {
@@ -25,9 +25,9 @@ export class AuthService {
     return this.http.post<any>(`${this.apiUrl}/login`, body).pipe(
       tap(response => {
         //Guardar token y calcular expiración
-        if (response && response.token) { 
+        if (response && response.token) {
           localStorage.setItem('token', response.token);
-          
+
           // Calculamos expiración: 30 minutos desde ahora (según tu middleware)
           const expirationDate = new Date(Date.now() + 30 * 60 * 1000);
           localStorage.setItem('token_exp', expirationDate.toString());
@@ -55,7 +55,7 @@ export class AuthService {
   // 2. MÉTODO PARA OBTENER FECHA: Decodifica el token (ejemplo simple)
   getTokenExpirationDate(): Date | null {
     if (!isPlatformBrowser(this.platformId)) return null; // 👈 ESTO EVITA EL ERROR
-    
+
     const exp = localStorage.getItem('token_exp');
     return exp ? new Date(exp) : null;
   }
