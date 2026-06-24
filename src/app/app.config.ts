78e9699fa21c675a91dd94b728c2b999d8b774f6
IDGS-803-23000643 <IDGS-803-23000643@outlook.com>
2026-06-24 15:10:55 -0600

    Correccion de rutas y servidor

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,21 +1,18 @@
 import { ApplicationConfig, provideBrowserGlobalErrorListeners } from '@angular/core';
 import { provideRouter } from '@angular/router';
-import { provideHttpClient, withInterceptors, withFetch } from '@angular/common/http';
+import { provideHttpClient, withInterceptors,withFetch } from '@angular/common/http';
 
 import { routes } from './app.routes';
 import { provideClientHydration, withEventReplay } from '@angular/platform-browser';
 import { authInterceptor } from './core/auth.interceptor';
 import { tokenInterceptor } from './core/token.interceptor';
 import { loaderInterceptor } from './core/loader.interceptor';
-import { provideLucideIcons } from '@lucide/angular';
-import { ALL_ICONS } from './shared/icon-map';
 
 export const appConfig: ApplicationConfig = {
   providers: [
     provideBrowserGlobalErrorListeners(),
     provideRouter(routes),
     provideClientHydration(withEventReplay()),
-    provideHttpClient(withFetch(), withInterceptors([tokenInterceptor, authInterceptor, loaderInterceptor])),
-    provideLucideIcons(...ALL_ICONS)
+    provideHttpClient(withFetch(), withInterceptors([tokenInterceptor, authInterceptor, loaderInterceptor]))
   ]
 };
